fc6d0307e8bdc9712c1a5d687c97972d1d4aa3e0
Your Name <chris.hipple@gmail.com>
2025-09-02 22:43:57 -0400

    lint

--- src/parsers/utils.rs
+++ src/parsers/utils.rs
@@ -41,9 +41,17 @@ pub fn parse_header(header: &str) -> Option<(u16, u16, u16, u16)> {
     let re = Regex::new(r"@@ -(\d+)(,(\d+))? \+(\d+)(,(\d+))? @@").unwrap();
     if let Some(caps) = re.captures(header) {
         let old_start = caps[1].parse::<u16>().unwrap();
-        let old_lines = caps.get(3).map_or("1", |m| m.as_str()).parse::<u16>().unwrap();
+        let old_lines = caps
+            .get(3)
+            .map_or("1", |m| m.as_str())
+            .parse::<u16>()
+            .unwrap();
         let new_start = caps[4].parse::<u16>().unwrap();
-        let new_lines = caps.get(6).map_or("1", |m| m.as_str()).parse::<u16>().unwrap();
+        let new_lines = caps
+            .get(6)
+            .map_or("1", |m| m.as_str())
+            .parse::<u16>()
+            .unwrap();
         return Some((old_start, old_lines, new_start, new_lines));
     }
     None
